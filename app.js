3946d495d56ebb9e3da5a25395c0310147c8db24
mstfrs <87162159+mstfrs@users.noreply.github.com>
2022-03-28 22:29:24 +0300

    Add files via upload
    Project repo moved

---
+++ app.js
@@ -0,0 +1,32 @@
+
+// let sum1=0;
+// let sum2=1;
+// let sum3=0;
+// while (sum3<100) {
+//     sum3=sum1+sum2;
+//     sum1=sum2;
+//     sum2=sum3;
+//     console.log(sum3);
+    
+// }
+
+// function fibonacci(x) {  
+// let sum1=0,sum2=1,total=0;
+// for (let i = 0; i < x; i++) {
+//     total =sum1+sum2;
+//     sum1=sum2;
+//     sum2=total;     
+// }
+// console.log(total) 
+// }
+// fibonacci(7)
+// fibonacci(12)
+
+// const x = 6 % 2;
+// const y = x ? 'One': 'Two';
+
+// console.log(y)
+// console.log(x)
+
+let firstNum = document.getElementById('firstnumber');
+console.log(firstNum);
